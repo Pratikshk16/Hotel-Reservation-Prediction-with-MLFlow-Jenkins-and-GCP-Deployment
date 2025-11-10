7b4b21aa7d821085fa8bd52bc1977a2362cc8128
Pratik Suchak <you@example.com>
2025-11-10 15:05:34 +0530

    reduced parameters for faster prodcution

--- config/model_param.py
+++ config/model_param.py
@@ -1,18 +1,18 @@
 from scipy.stats import randint, uniform
 
 LIGHTGBM_PARAMS = {
-    'n_estimators' : randint(100, 500),
-    'learning_rate' : uniform(0.01, 0.2),
-    'num_leaves' : randint(20, 100),
-    'max_depth' : randint(5, 50),
-    'boosting_type' : ['gbdt', 'dart', 'goss']
+    'n_estimators' : randint(100, 300),
+    'learning_rate' : uniform(0.03, 0.15),
+    'num_leaves' : randint(20, 60),
+    'max_depth' : randint(5, 20),
+    'boosting_type' : ['gbdt']
 }
 
 
 RANDOM_SEARCH_PARAMS = {
-    'n_iter' : 5,
+    'n_iter' : 2,
     'cv' : 2,
-    'verbose' : 2,
+    'verbose' : 1,
     'n_jobs' : -1,
     'random_state' : 42,
     'scoring' : 'accuracy'
